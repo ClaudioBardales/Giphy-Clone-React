dcde5c6eebd0f21dbaa620e057fe1976dd4e4e70
Claudio <claudio@Claudios-MacBook-Pro.local>
2021-06-07 20:20:10 -0700

    Adding styles

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,51 @@
 import React from "react";
+import giphy from "../img/giphy.png";
 import Styled from "styled-components";
 
 const Search = () => {
+  console.log(giphy);
   return (
-    <div>
-      <StyledHeader>Giphy Clone</StyledHeader>
+    <Container>
+      <StyledImg src={giphy} />
       <StyledForm>
-        <input type="text" placeholder="Search" />
-        <input type="submit" />
+        <StyledInput type="text" />
+        <SubmitButton type="submit" />
       </StyledForm>
-    </div>
+    </Container>
   );
 };
 
-const StyledHeader = Styled.h1`
-width: 55%;
+const Container = Styled.div`
+width: 50%;
+display: flex;
 margin: auto;
-text-align: center;
+flex-direction: column;
+`;
+
+const StyledImg = Styled.img`
+width: 90%;
+margin: auto;
 `;
 
 const StyledForm = Styled.form`
 display: flex;
 justify-content: center;
+align-items: center;
+padding-top: 1.5rem;
+`;
+
+const StyledInput = Styled.input`
+width: 50%;
+height: 31.6px;
+`;
+
+const SubmitButton = Styled.input`
+background-color: #c147c3;
+font-size: 1.5rem;
+border: 0;
+padding-top: 5px;
+padding-bottom: 5px;
+color: white;
 `;
 
 export default Search;
