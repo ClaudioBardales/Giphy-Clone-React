3bb3d6808fcc708ff20575b2296e6a7b9e3775b4
Claudio <claudio@Claudios-MacBook-Pro.local>
2021-06-21 21:06:37 -0700

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import GlobalStyle from "./components/GlobalStyles";
-import Search from "./components/Search";
 import Giphy from "./components/Giphy";
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
-      <Search />
       <Giphy />
     </div>
   );
--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
+import Styled from "styled-components";
 import axios from "axios";
+import giphy from "../img/giphy.png";
 
 function Giphy() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const results = await axios("https://api.giphy.com/v1/gifs/trending", {
         params: {
           api_key: "bo7l8y4GctiRWvN7WQKm6NKC2TrmJEjc",
+          limit: 15,
         },
       });
       console.log(results);
@@ -21,13 +25,85 @@ function Giphy() {
     return data.map((el) => {
       return (
         <div key={el.id}>
-          <img src={el.images.fixed_height.url} />
+          <img src={el.images.fixed_width.url} />
         </div>
       );
     });
   };
 
-  return <div>{renderGifs()}</div>;
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const results = await axios("https://api.giphy.com/v1/gifs/search", {
+      params: {
+        api_key: "bo7l8y4GctiRWvN7WQKm6NKC2TrmJEjc",
+        q: search,
+      },
+    });
+    setData(results.data.data);
+  };
+
+  return (
+    <div>
+      <Container>
+        <StyledImg src={giphy} />
+        <StyledForm>
+          <StyledInput
+            value={search}
+            onChange={handleSearchChange}
+            type="text"
+          />
+          <SubmitButton onClick={handleSubmit} type="submit" />
+        </StyledForm>
+      </Container>
+      <Grid>{renderGifs()}</Grid>
+    </div>
+  );
 }
+
+const Grid = Styled.div`
+display: grid;
+grid-template-columns: repeat(3,1fr);
+align-items: center;
+justify-items: center;
+
+
+`;
+
+const Container = Styled.div`
+width: 50%;
+display: flex;
+margin: auto;
+flex-direction: column;
+`;
+
+const StyledImg = Styled.img`
+width: 90%;
+margin: auto;
+`;
+
+const StyledForm = Styled.form`
+display: flex;
+justify-content: center;
+align-items: center;
+padding-top: 1.5rem;
+`;
+
+const StyledInput = Styled.input`
+width: 50%;
+height: 31.6px;
+`;
+
+const SubmitButton = Styled.input`
+background-color: #c147c3;
+font-size: 1.5rem;
+border: 0;
+padding-top: 5px;
+padding-bottom: 5px;
+color: white;
+`;
 
 export default Giphy;
